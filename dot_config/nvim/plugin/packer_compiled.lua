dfb659b5f77e3330370593da622c064cc89d1032
kaine0x00 <dannyrobrom@hotmail.com>
2024-04-24 13:37:12 +0200

    fixed nvim

--- dot_config/nvim/plugin/packer_compiled.lua
+++ dot_config/nvim/plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/kaine/.cache/nvim/packer_hererocks/2.1.1702233742/share/lua/5.1/?.lua;/home/kaine/.cache/nvim/packer_hererocks/2.1.1702233742/share/lua/5.1/?/init.lua;/home/kaine/.cache/nvim/packer_hererocks/2.1.1702233742/lib/luarocks/rocks-5.1/?.lua;/home/kaine/.cache/nvim/packer_hererocks/2.1.1702233742/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/kaine/.cache/nvim/packer_hererocks/2.1.1702233742/lib/lua/5.1/?.so"
+local package_path_str = "/home/kaine/.cache/nvim/packer_hererocks/2.1.1713773202/share/lua/5.1/?.lua;/home/kaine/.cache/nvim/packer_hererocks/2.1.1713773202/share/lua/5.1/?/init.lua;/home/kaine/.cache/nvim/packer_hererocks/2.1.1713773202/lib/luarocks/rocks-5.1/?.lua;/home/kaine/.cache/nvim/packer_hererocks/2.1.1713773202/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/kaine/.cache/nvim/packer_hererocks/2.1.1713773202/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -113,6 +113,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/kaine/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
+  },
+  ["mdx.nvim"] = {
+    loaded = true,
+    path = "/home/kaine/.local/share/nvim/site/pack/packer/start/mdx.nvim",
+    url = "https://github.com/davidmh/mdx.nvim"
   },
   ["nvim-cmp"] = {
     loaded = true,
